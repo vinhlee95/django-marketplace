6347f0962cc236941d6dac9b985126ce538b9aff
Vinh Le <vinh@vertics.co>
2020-10-26 11:18:15 +0200

    feat: add admin models

--- home_marketplace/listings/admin.py
+++ home_marketplace/listings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+	list_display_links = ('id', 'title')
 
 # Register your models here.
+admin.site.register(Listing, ListingAdmin)
