9a3eebf337f282cf05e4fdb24c4e75f770b93c20
Vinh Le <vinh@vertics.co>
2020-10-27 10:28:42 +0200

    feat: add filter and search fields

--- home_marketplace/listings/admin.py
+++ home_marketplace/listings/admin.py
@@ -4,6 +4,9 @@ from .models import Listing
 class ListingAdmin(admin.ModelAdmin):
 	list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
 	list_display_links = ('id', 'title')
+	list_filter = ('realtor',)
+	list_editable = ('is_published',)
+	search_fields = ('id', 'title', 'price')
 
 # Register your models here.
 admin.site.register(Listing, ListingAdmin)
